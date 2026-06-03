976c5c79857d7a4db98233e56759c2f4d5988e7a
Ashil-James <ashilgjames@gmail.com>
2026-06-03 16:29:25 +0530

    feat: initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "*",
+    credentials: true,
+  }),
+);
+
+app.use(
+  express.json({
+    limit: "10mb",
+  }),
+);
+
+app.use(
+  express.urlencoded({
+    limit: "10mb",
+    extended: true,
+  }),
+);
+
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app };
