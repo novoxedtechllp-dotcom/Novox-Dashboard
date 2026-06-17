8f614ec07a24670778b463fc0ea4886089e49142
Ashil-James <ashilgjames@gmail.com>
2026-06-17 15:44:51 +0530

    feat: keep-alive render

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import { app } from "./app.js";
 import { startAttendanceCron } from "./cron/attendance.cron.js";
+import { startKeepAliveCron } from "./cron/keepAlive.cron.js";
 
 const PORT = process.env.PORT || 5000;
 
 // Start background cron jobs
 startAttendanceCron();
+startKeepAliveCron();
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
