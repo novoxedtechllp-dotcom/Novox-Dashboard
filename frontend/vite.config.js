3655ad1c78f6f508e67c4e0d618a0862a7002dc2
sajad yoosuf <sajadyoosuf.dev@gmail.com>
2026-06-26 12:57:05 +0530

    feat: configure Vite with React, Tailwind CSS, and API proxy settings

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://novox-dashboard.onrender.com',
+        target: 'https://novox-dashboard-w7d5.onrender.com',
         changeOrigin: true,
         secure: false,
       },
